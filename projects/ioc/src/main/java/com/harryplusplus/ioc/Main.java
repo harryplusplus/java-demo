9a96f2aa7109c9fa512d12922643afdd1ca7d90e
harryplusplus <harryplusplus@gmail.com>
2025-08-30 05:58:37 +0000

    chore: Change class call name

--- projects/ioc/src/main/java/com/harryplusplus/ioc/Main.java
+++ projects/ioc/src/main/java/com/harryplusplus/ioc/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is a IoC Demo!");
         final var container = new Container();
-        container.register(new Main.Foo());
-        container.register(new Main.Foo());
+        container.register(new Foo());
+        container.register(new Foo());
     }
 }
